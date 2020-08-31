434244284acf9984ffc16e327479591f63f167bf
demonly <ly18824110284@gmail.com>
2020-08-31 15:58:03 +0800

    init: 初始化 test 分支

--- web/src/index.js
+++ web/src/index.js
@@ -13,7 +13,7 @@ export default class App extends React.Component {
     return (
       <div>
         <div>
-          branch: master
+          branch: test
         </div>
         <div>
           server status:
